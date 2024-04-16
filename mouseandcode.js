e240b4f71bf46ce537e413e567d0154f514c2618
macosta2015 <macosta2015@my.fit.edu>
2024-04-16 00:55:01 -0400

    Update mouseandcode.js

--- mouseandcode.js
+++ mouseandcode.js
@@ -124,31 +124,49 @@ const { performRightClickOptionByTitle } = require('./components/performRightCli
 
 
 
+        //SELECT SKETCH TO CLICK OR UNCLICK, THIS CODE IS REALLY IMPORTANT
+        console.log('Waiting 10 seconds.');
+        console.log('SELECTING ITEM 5 ON THE LIST.');
+        await new Promise(resolve => setTimeout(resolve, 10000)); // Wait for 10 seconds
+        await newPage.evaluate(() => {
+            const thirdButton = document.querySelectorAll('.os-list-item-name')[5];
+            thirdButton.click();
+            if (thirdButton) {
+                thirdButton.scrollIntoView({ behavior: 'smooth', block: 'center', inline: 'center' });
+            } else {
+                console.error('Third button not found.');
+            }
+        });
+
 
 
 
 
         // /////////
-        //         //EDIT
-        //         console.log('START clickButtonByIndex');
-        //         console.log('WE START THE 10TH PHASE');
-        //         await new Promise(resolve => setTimeout(resolve, 10000)); // Wait for 5
-        //         //ALL THE CODE TOGETHER
-        //         const desiredIndex3 = 8; // Or any other desired index
-        //         await clickButtonByIndex(newPage, desiredIndex3);
-        //         //Right click option
-        //         //EDIT
-        //         console.log('END clickButtonByIndex');
-        //         console.log('WE START THE 10TH PHASE');
-        //         await new Promise(resolve => setTimeout(resolve, 10000)); // Wait for 5
-        //         const selector3 = 'div[data-id="yyHiDTMWflCzpmJN"]'; // Replace with the appropriate selector
-        //         const title3 = 'Third Sketch'; // Replace with the desired title
-        //         const editOptions4 = await performRightClickOptionByTitle(newPage, selector3, title3);
-        //         console.log(editOptions4);
-        //         //NEED TO ADD THE EDIT FUNCTION HERE, LIKE pasteIntoSketchFunction OR copySketchFunction
-        //         editIntoSketchFunction(editOptions4, newPage);
-        //         await new Promise(resolve => setTimeout(resolve, 40000)); // Wait for 10 seconds
-        //         console.log('Waited for 40 seconds.'); console.log('AFTER THE CODE RUNS.');
+        //EDIT
+        console.log('START clickButtonByIndex');
+        console.log('WE START THE 10TH PHASE');
+        await new Promise(resolve => setTimeout(resolve, 10000)); // Wait for 5
+        //ALL THE CODE TOGETHER
+        const desiredIndex3 = 7; // Or any other desired index
+        await clickButtonByIndex(newPage, desiredIndex3);
+        //Right click option
+        //EDIT
+        console.log('END clickButtonByIndex');
+        console.log('WE START THE 10TH PHASE');
+        await new Promise(resolve => setTimeout(resolve, 10000)); // Wait for 5
+        // const selector3 = 'div[data-id="yyHiDTMWflCzpmJN"]'; // Replace with the appropriate selector //ORIGINAL
+        const selector3 = 'div[data-id="AJC+8X/uU1MWWXEK"]'; // Replace with the appropriate selector
+
+        // const title3 = 'Third Sketch'; // Replace with the desired title
+        const title3 = 'Second Sketch'; // Replace with the desired title
+
+        const editOptions4 = await performRightClickOptionByTitle(newPage, selector3, title3);
+        console.log(editOptions4);
+        //NEED TO ADD THE EDIT FUNCTION HERE, LIKE pasteIntoSketchFunction OR copySketchFunction
+        editIntoSketchFunction(editOptions4, newPage);
+        await new Promise(resolve => setTimeout(resolve, 40000)); // Wait for 10 seconds
+        console.log('Waited for 40 seconds.'); console.log('AFTER THE CODE RUNS.');
 
 
 
@@ -181,43 +199,45 @@ const { performRightClickOptionByTitle } = require('./components/performRightCli
 
 
 
-        //         //COMMAND FOR SEARCHING TRANSFORM AND THEN CLICKING ON IT 
-        //         console.log('//COMMAND FOR SEARCHING TRANSFORM AND THEN CLICKING ON IT ');
-        //         await newPage.click('button.command-search-trigger');
-        //         await new Promise(resolve => setTimeout(resolve, 3000));
-        //         console.log('Waited for 3 seconds.');
+        //COMMAND FOR SEARCHING TRANSFORM AND THEN CLICKING ON IT 
+        await new Promise(resolve => setTimeout(resolve, 10000));
+        console.log('//COMMAND FOR SEARCHING TRANSFORM AND THEN CLICKING ON IT, waiting 10 seconds');
+        console.log('Waited for 10 seconds.');
 
-        //         await newPage.type('.os-search-box-input', 'transform');
-        //         await new Promise(resolve => setTimeout(resolve, 3000));
-        //         console.log('Typed "transform" into the search input field.');
+        await newPage.click('button.command-search-trigger');
+        await new Promise(resolve => setTimeout(resolve, 10000));
+        console.log('Waited for 10 seconds.');
 
-        //         await newPage.keyboard.press('Enter');
-        //         await newPage.mouse.move(290, 311);
-        //         await newPage.mouse.down({ button: 'left' });
-        //         console.log('Mouse clicked and held at X:290, Y:311');
-        //         await new Promise(resolve => setTimeout(resolve, 3000));
+        await newPage.type('.os-search-box-input', 'transform');
+        await new Promise(resolve => setTimeout(resolve, 3000));
+        console.log('Typed "transform" into the search input field.');
 
-        //         const readline = require('readline');
-        //         const rl = readline.createInterface({ input: process.stdin, output: process.stdout });
-        //         console.log('Please press the Enter key to continue...');
-        //         const enterPromise = new Promise(resolve => rl.once('line', resolve));
-        //         await enterPromise;
-        //         console.log('User pressed Enter to continue.');
+        await newPage.keyboard.press('Enter');
+        await newPage.mouse.move(290, 311);
+        await newPage.mouse.down({ button: 'left' });
+        console.log('Mouse clicked and held at X:290, Y:311');
+        await new Promise(resolve => setTimeout(resolve, 3000));
 
-        //         await newPage.evaluate(() => {
-        //             document.addEventListener('mousemove', (event) => {
-        //                 console.log(`Mouse coordinates: X = ${event.clientX}, Y = ${event.clientY}`);
-        //             });
-        //         });
+        const readline = require('readline');
+        const rl = readline.createInterface({ input: process.stdin, output: process.stdout });
+        console.log('Please press the Enter key to continue...');
+        const enterPromise = new Promise(resolve => rl.once('line', resolve));
+        await enterPromise;
+        console.log('User pressed Enter to continue.');
 
-        //         console.log('Move the mouse over the page to see the coordinates...');
-        //         await new Promise(resolve => setTimeout(resolve, 3000));
+        await newPage.evaluate(() => {
+            document.addEventListener('mousemove', (event) => {
+                console.log(`Mouse coordinates: X = ${event.clientX}, Y = ${event.clientY}`);
+            });
+        });
 
-        //         console.log('Script completed successfully.');
+        console.log('Move the mouse over the page to see the coordinates...');
+        await new Promise(resolve => setTimeout(resolve, 3000));
+
+        console.log('Script completed successfully.');
 
 
 
-        //////
     } catch (error) {
         console.error('An error occurred:', error);
     }
